b56416e56a003dcad066fa380db0932b96f49f97
Viiralt <viiralt.h@gmail.com>
2018-07-09 19:26:07 +0200

    feat: add carousel

--- src/components/styled/Container.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Container = styled.View`
-  flex: 1;
-  align-items: center;
-  justify-content: center;
-`;
-
-export default Container;
--- src/components/styled/Wrapper.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-const Wrapper = styled.SafeAreaView`
-  flex: 1;
-`;
-
-export default Wrapper;
